dd339b92d3e407cfc824663a124a0c019005d034
manueldevmx <43160136+manueldevmx@users.noreply.github.com>
2022-03-02 19:03:19 -0600

    se creo Screen Contraseña

---
+++ app/src/main/java/com/honeykoders/bankodemia/contrasena.kt
@@ -0,0 +1,11 @@
+package com.honeykoders.bankodemia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class contrasena : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contrasena)
+    }
+}
