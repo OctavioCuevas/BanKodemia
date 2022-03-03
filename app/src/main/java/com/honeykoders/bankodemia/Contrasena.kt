62a4e0f9f6ba58e3ec49afe78795dcb43ba2d595
manueldevmx <43160136+manueldevmx@users.noreply.github.com>
2022-03-02 21:31:24 -0600

    se añadieron imagenes bk y bankodemia logos

--- app/src/main/java/com/honeykoders/bankodemia/Contrasena.kt
+++ app/src/main/java/com/honeykoders/bankodemia/Contrasena.kt
@@ -2,10 +2,13 @@ package com.honeykoders.bankodemia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.DrawableRes
 
-class contrasena : AppCompatActivity() {
+class Contrasena : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contrasena)
     }
+
+
 }
