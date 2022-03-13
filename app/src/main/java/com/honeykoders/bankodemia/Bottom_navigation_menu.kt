b631807f5d8381a10c54d0e014d158cdbdc91912
manueldevmx <43160136+manueldevmx@users.noreply.github.com>
2022-03-13 00:34:01 -0600

    se creo carpetas de arquitectura MVVM

---
+++ app/src/main/java/com/honeykoders/bankodemia/Bottom_navigation_menu.kt
@@ -0,0 +1,36 @@
+package com.honeykoders.bankodemia
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.honeykoders.bankodemia.databinding.ActivityBottomNavigationMenuBinding
+
+class Bottom_navigation_menu : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBottomNavigationMenuBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityBottomNavigationMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController =
+            findNavController(R.id.nav_host_fragment_activity_bottom_navigation_menu)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
