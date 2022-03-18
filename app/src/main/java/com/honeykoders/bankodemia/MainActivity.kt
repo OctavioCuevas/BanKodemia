085ee22c093ae3daceeade9a0f2db9a291819b80
shirousora <soraya.islas@alumnos.udg.mx>
2022-03-17 22:57:11 -0600

    next 3 screens, only layout designs , contacts list, add new contacts and end adding contacts on fragments. there's no functions applied yet. item for contacts list for the recycler view

--- app/src/main/java/com/honeykoders/bankodemia/MainActivity.kt
+++ app/src/main/java/com/honeykoders/bankodemia/MainActivity.kt
@@ -2,10 +2,18 @@ package com.honeykoders.bankodemia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btn_newAccount: Button
+    private lateinit var btn_login: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //init()
     }
+
 }
