9a465db68f0700d9b75ae679a67901aee337baa5
shirousora <soraya.islas@alumnos.udg.mx>
2022-03-21 12:10:02 -0600

    se modificó breve logica de nuevo destinatario.

--- app/src/main/java/com/honeykoders/bankodemia/addNewContact.kt
+++ app/src/main/java/com/honeykoders/bankodemia/addNewContact.kt
@@ -5,27 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.honeykoders.bankodemia.databinding.FragmentAddNewContactBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [addNewContact.newInstance] factory method to
- * create an instance of this fragment.
- */
 class addNewContact : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentAddNewContactBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        binding.btnClabe.setOnClickListener {
+            binding.tvAddContAcNumb.setText(R.string.tarjetatxttv)
+            //funcion para validar 16 digitos de tarjeta
+            validarDigitosT()
+        }
+    }
+
+    private fun validarDigitosT() {
+        val caracteres:Int = binding.tietAddContAcNumb.text!!.length
+        if(caracteres<18){
+            binding.tilAddContAcNumb.setError(getString(R.string.errorlimitnumclabe))
         }
     }
 
@@ -34,26 +35,11 @@ class addNewContact : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_new_contact, container, false)
+        _binding = FragmentAddNewContactBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment addNewContact.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            addNewContact().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
